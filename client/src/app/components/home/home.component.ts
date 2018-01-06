0dc5eb8f69c26403b1be34e23e358029e6dbda15
biran1 <biransh@gmail.com>
2018-01-07 00:05:48 +0200

    update home.component.html

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -8,11 +8,18 @@ import { AuthService } from '../../services/auth.service';
 })
 export class HomeComponent implements OnInit {
 
+  username;
+  
+
   constructor(
     public authService: AuthService
   ) { }
 
   ngOnInit() {
+        // Get profile username on page load
+        this.authService.getProfile().subscribe(profile => {
+          this.username = profile.user.username; // Used when creating new blog posts and comments
+        });
   }
 
 }
